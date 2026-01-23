3dd8f1f62f3ab023c63bb3832434c50e43b619f9
Will Griffin <willgriffin@gmail.com>
2026-01-23 12:23:27 -0700

    fix: revert unintended smrt -> Smrt export rename

--- packages/smrt-svelte/src/components/ai/index.ts
+++ packages/smrt-svelte/src/components/ai/index.ts
@@ -6,5 +6,5 @@ export { default as AILoadingOverlay } from './AILoadingOverlay.svelte';
 export { default as CapabilityGate } from './CapabilityGate.svelte';
 export { default as DownloadProgress } from './DownloadProgress.svelte';
 export { default as STTTest } from './STTTest.svelte';
-export { default as Smrt } from './smrt.svelte';
+export { default as smrt } from './smrt.svelte';
 export { default as VoiceInput } from './VoiceInput.svelte';
